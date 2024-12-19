371616e2809b12b32bdc70b3377cf841851df49b
Oskar Kobza <oskarkobza@gmail.com>
2024-12-19 18:35:35 +0100

    Adjustments for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,16 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     server: {
-        host: 'localhost',
-        port: 5173,
+        host: '0.0.0.0', // Użycie localhost zamiast IPv6
+        port: 5174,        // Domyślny port
+        cors: {
+            origin: 'http://storytime.localhost', // Specyfikacja dozwolonego pochodzenia
+            methods: ['GET', 'POST', 'PUT', 'DELETE'],
+            credentials: true,
+        },
         hmr: {
             host: 'localhost',
+            port: 5174,
         },
     },
     plugins: [
